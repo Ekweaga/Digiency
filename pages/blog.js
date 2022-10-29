c682783a29c104034c725dc7fa8ce71ea98974ce
Ekweaga <hikay133@gmail.com>
2022-10-29 13:23:42 +0100

    Home page done

---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Blog() {
+  return (
+    <div className='bg-[#FFF7F4] md:h-[200px] relative p-4 text-center '>
+      <h1 className='text-3xl font-bold'>Blog</h1>
+    </div>
+  )
+}
+
+export default Blog
